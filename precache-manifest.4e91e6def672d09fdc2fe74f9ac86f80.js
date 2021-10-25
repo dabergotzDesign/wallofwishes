fcc9be46bcc8d3370c920f6fe23cd3dbca9ed33d
dabergotzDesign <otakulp@gmx.de>
2021-10-25 11:51:58 +0200

    Updates

---
+++ precache-manifest.4e91e6def672d09fdc2fe74f9ac86f80.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760fe99c03e57fb07bc5adf0b997b8c4",
+    "url": "/wallofwishes/index.html"
+  },
+  {
+    "revision": "adbe2f5254764243317f",
+    "url": "/wallofwishes/static/css/main.5ed50d6d.chunk.css"
+  },
+  {
+    "revision": "6ff6a75f0416ea682b04",
+    "url": "/wallofwishes/static/js/2.72371660.chunk.js"
+  },
+  {
+    "revision": "069b07e450ee21bbc09ec2720cf6e971",
+    "url": "/wallofwishes/static/js/2.72371660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25167c29d8932e7c41d",
+    "url": "/wallofwishes/static/js/3.5e18d650.chunk.js"
+  },
+  {
+    "revision": "adbe2f5254764243317f",
+    "url": "/wallofwishes/static/js/main.98322a67.chunk.js"
+  },
+  {
+    "revision": "d55a3cc95c1041a121e8",
+    "url": "/wallofwishes/static/js/runtime-main.4af76542.js"
+  },
+  {
+    "revision": "b6b540ec63bc81cd59f0633c4d3dd0cc",
+    "url": "/wallofwishes/static/media/00-Empty.b6b540ec.png"
+  },
+  {
+    "revision": "8d8dd63e8b7407c63215dc458460df7e",
+    "url": "/wallofwishes/static/media/AA-Fish_normal.8d8dd63e.png"
+  },
+  {
+    "revision": "39b53ea835db06c8f39cd4978b4df740",
+    "url": "/wallofwishes/static/media/AB-Fish_circle.39b53ea8.png"
+  },
+  {
+    "revision": "14959bb6cff14ef013f9cf1809515d81",
+    "url": "/wallofwishes/static/media/AC-Fish_yingyang.14959bb6.png"
+  },
+  {
+    "revision": "6b46d03ee36f2b4492b1d5f486c646e5",
+    "url": "/wallofwishes/static/media/AD-Fish_jumpup.6b46d03e.png"
+  },
+  {
+    "revision": "72ca0cdf0e4a095b6be550a398a47039",
+    "url": "/wallofwishes/static/media/Ahamkara_Egg.72ca0cdf.png"
+  },
+  {
+    "revision": "caf359d369dfe3b0993d07fac51e0e2c",
+    "url": "/wallofwishes/static/media/BA-Snake_infinite.caf359d3.png"
+  },
+  {
+    "revision": "d0c0c8df092f402d34f97d0514cccd6b",
+    "url": "/wallofwishes/static/media/BB-Snake_eight.d0c0c8df.png"
+  },
+  {
+    "revision": "1183af0cf293f5794be1f726b43e5135",
+    "url": "/wallofwishes/static/media/BC-Snake_Sform.1183af0c.png"
+  },
+  {
+    "revision": "bc8185262e61d8bfba60cb2cb1b86888",
+    "url": "/wallofwishes/static/media/BD-Snake_mustache.bc818526.png"
+  },
+  {
+    "revision": "45a18067c8bad076808d51c3e22b7180",
+    "url": "/wallofwishes/static/media/CA-Dragon_fireleft.45a18067.png"
+  },
+  {
+    "revision": "28c028896ce761383963f8cd0169941e",
+    "url": "/wallofwishes/static/media/CB-Dragon_fireright.28c02889.png"
+  },
+  {
+    "revision": "d13446e22d3aa0514db3f17e0e281827",
+    "url": "/wallofwishes/static/media/CC-Dragon_spearneck.d13446e2.png"
+  },
+  {
+    "revision": "dfd41efdb144f24284e85a9eacda2168",
+    "url": "/wallofwishes/static/media/CD-Dragon_spearclaws.dfd41efd.png"
+  },
+  {
+    "revision": "a4b8ce9c779630c44f61ab666eed4550",
+    "url": "/wallofwishes/static/media/D2-Drifter.a4b8ce9c.jpg"
+  },
+  {
+    "revision": "70bf8c900d947f5bd3cde9f1172a8dc1",
+    "url": "/wallofwishes/static/media/DA-Bird_falling.70bf8c90.png"
+  },
+  {
+    "revision": "6224154e40cd5ebd3155b07d2e11a808",
+    "url": "/wallofwishes/static/media/DB-Bird_tree.6224154e.png"
+  },
+  {
+    "revision": "d8e3b2f9ecf7369ea55a687dfff2f70f",
+    "url": "/wallofwishes/static/media/DC-Bird_fly.d8e3b2f9.png"
+  },
+  {
+    "revision": "37d856332978c003842f583425454257",
+    "url": "/wallofwishes/static/media/DD-Bird_walk.37d85633.png"
+  },
+  {
+    "revision": "5c16c26e2b0d6248cc057a51b198d1f4",
+    "url": "/wallofwishes/static/media/Drifter.5c16c26e.ogg"
+  },
+  {
+    "revision": "07af4ccf8b7f17188e01bde90d2d3c30",
+    "url": "/wallofwishes/static/media/Failsafe.07af4ccf.ogg"
+  },
+  {
+    "revision": "4923f4adbba4b4a2c1b4b201d38ddad6",
+    "url": "/wallofwishes/static/media/Failsafe.4923f4ad.webp"
+  },
+  {
+    "revision": "2cc095adadc25a61811e5eb500d3f840",
+    "url": "/wallofwishes/static/media/HopeForTheFuture.2cc095ad.ogg"
+  },
+  {
+    "revision": "ac8f81c851bfd04be50631f5d0c7e02d",
+    "url": "/wallofwishes/static/media/Morgeth_720p.ac8f81c8.mp4"
+  },
+  {
+    "revision": "1958170a3ad03b8c5edc03d738979549",
+    "url": "/wallofwishes/static/media/Riven_720p.1958170a.mp4"
+  },
+  {
+    "revision": "35dcb01386b88baae21ce036682a6231",
+    "url": "/wallofwishes/static/media/ShuroChi_720p.35dcb013.mp4"
+  },
+  {
+    "revision": "6b0fee5a3185f0f37d6a1815e9938b71",
+    "url": "/wallofwishes/static/media/Vault_720p.6b0fee5a.mp4"
+  },
+  {
+    "revision": "b2399e3cd5c98dfede505b26428c7753",
+    "url": "/wallofwishes/static/media/button_ornament.b2399e3c.png"
+  },
+  {
+    "revision": "5fd06a3b094acb26ec3de41e9ee8d358",
+    "url": "/wallofwishes/static/media/chest_sfx.5fd06a3b.ogg"
+  },
+  {
+    "revision": "333ae479485b089c33116e656cea3ce3",
+    "url": "/wallofwishes/static/media/chest_spritesheet.333ae479.png"
+  },
+  {
+    "revision": "7b41269d2bfee3837265d4c9a4258416",
+    "url": "/wallofwishes/static/media/darknessGhost_spritesheet.7b41269d.png"
+  },
+  {
+    "revision": "bbdb87eb28abf68c0c15c6b1353af5ef",
+    "url": "/wallofwishes/static/media/darknessZone.bbdb87eb.png"
+  },
+  {
+    "revision": "e98947c41cfbb9987fbbd1a474bff939",
+    "url": "/wallofwishes/static/media/emblem_banner_power.e98947c4.jpg"
+  },
+  {
+    "revision": "66287d061a82be27d53d84d236144484",
+    "url": "/wallofwishes/static/media/emblem_banner_riven.66287d06.jpg"
+  },
+  {
+    "revision": "526a80a0f286d699a0eb4116daf1c3d7",
+    "url": "/wallofwishes/static/media/itemframe_ani.526a80a0.png"
+  },
+  {
+    "revision": "1b69ec27624d998090329df830343c45",
+    "url": "/wallofwishes/static/media/legendaryengram.1b69ec27.ogg"
+  },
+  {
+    "revision": "05115eccc8d52b06b619144db86eef12",
+    "url": "/wallofwishes/static/media/petra.05115ecc.ogg"
+  },
+  {
+    "revision": "5dafe171cba62521ca0390f7f5c06e57",
+    "url": "/wallofwishes/static/media/throne_room.5dafe171.jpg"
+  },
+  {
+    "revision": "970abb615581c9ded7cad88a50cb7276",
+    "url": "/wallofwishes/static/media/traveler.970abb61.webp"
+  },
+  {
+    "revision": "9454bbd6332fc457dcb75cfdfc789b87",
+    "url": "/wallofwishes/static/media/wall_ring.9454bbd6.png"
+  },
+  {
+    "revision": "a74895597da3a37581405bd11192c74d",
+    "url": "/wallofwishes/static/media/wishWall_background.a7489559.jpg"
+  },
+  {
+    "revision": "034e7a55669a3ed247053c56213afcf8",
+    "url": "/wallofwishes/static/media/wish_III_cs.034e7a55.png"
+  },
+  {
+    "revision": "be0dc7146306c754d93e2a73e423df62",
+    "url": "/wallofwishes/static/media/wish_II_cs.be0dc714.png"
+  },
+  {
+    "revision": "07a449a820904ce50692523a6869a2c6",
+    "url": "/wallofwishes/static/media/wish_IV_cs.07a449a8.png"
+  },
+  {
+    "revision": "27919c4863c44bd34443ad617cb34f6d",
+    "url": "/wallofwishes/static/media/wish_IX_cs.27919c48.png"
+  },
+  {
+    "revision": "8df83fd062d6481c138a0908866dbf79",
+    "url": "/wallofwishes/static/media/wish_I_cs.8df83fd0.png"
+  },
+  {
+    "revision": "3173a6c036ccc7b24a0ae9cf528aadf7",
+    "url": "/wallofwishes/static/media/wish_VIII_cs.3173a6c0.png"
+  },
+  {
+    "revision": "4bc1bdd45a082d8d63d61f2b500d927f",
+    "url": "/wallofwishes/static/media/wish_VII_cs.4bc1bdd4.png"
+  },
+  {
+    "revision": "dfa0c91479018d29bce3687cd45c10fd",
+    "url": "/wallofwishes/static/media/wish_VI_cs.dfa0c914.png"
+  },
+  {
+    "revision": "978d5626d211cac6bdab8b201e8df4ce",
+    "url": "/wallofwishes/static/media/wish_V_cs.978d5626.png"
+  },
+  {
+    "revision": "ed3b4fcaacd2fc9504d918a92fda172b",
+    "url": "/wallofwishes/static/media/wish_XIII_cs.ed3b4fca.png"
+  },
+  {
+    "revision": "96a3204629422c3cf505a92d54ff98a8",
+    "url": "/wallofwishes/static/media/wish_XII_cs.96a32046.png"
+  },
+  {
+    "revision": "c96baa717fb267aee88b1db18870b5fb",
+    "url": "/wallofwishes/static/media/wish_XIV_cs.c96baa71.png"
+  },
+  {
+    "revision": "89a8d7e9605f2aa4e871177843a38016",
+    "url": "/wallofwishes/static/media/wish_XI_cs.89a8d7e9.png"
+  },
+  {
+    "revision": "07401ff885bada5bf056f95de64f4246",
+    "url": "/wallofwishes/static/media/wish_XV_cs.07401ff8.png"
+  },
+  {
+    "revision": "1828b73b20f380d0d09d03b57dfc083e",
+    "url": "/wallofwishes/static/media/wish_X_cs.1828b73b.png"
+  }
+]);
